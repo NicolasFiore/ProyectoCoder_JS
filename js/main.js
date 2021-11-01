3ddbd36fe202c8a91ae5b070a85471c0e69220b9
NicolasFiore <91384707+NicolasFiore@users.noreply.github.com>
2021-10-31 22:44:33 -0300

    Add files via upload
    Empezando a usar eventos

--- js/main.js
+++ js/main.js
@@ -1,45 +1,33 @@
 // peso a euro y dolar
 function convertirPesoDolar(moneda)
 {
-    return moneda / dolarPeso; 
+    return moneda / cambio1.valor; 
 }
 
 function convertirPesoEuro(moneda)
 {
-    return moneda / euroPeso; 
+    return moneda / cambio2.valor; 
 }
 
 // dolar a peso y euro
 function convertirDolarPeso(moneda)
 {  
-    return moneda * dolarPeso; 
+    return moneda * cambio1.valor; 
 }
 function convertirDolarEuro(moneda)
 {
-    return moneda * dolarEuro; 
+    return moneda * cambio3.valor; 
 }
 
 // euro a peso y dolar
 function convertirEuroPeso(moneda)
 {
-    return moneda * euroPeso; 
+    return moneda * cambio2.valor; 
 }
 function convertirEuroDolar(moneda)
 {
-    return moneda * euroDolar; 
+    return moneda * cambio4.valor; 
 }
-
-// Variables
-let pesos;
-let dolares;
-let euros;
-let opcion;
-let fecha = document.getElementById("fecha");
-// Precios del cambio
-let dolarPeso = 98.91;
-let euroPeso = 114.28;
-let dolarEuro = 0.87;
-let euroDolar = 1.17;
 
 // Constructor de objetos
 function Cambio(nombre, valor){
@@ -53,42 +41,40 @@ const cambio2 = new Cambio("euro a peso", 114.28 );
 const cambio3 = new Cambio("dolar a euro", 0.87);
 const cambio4 = new Cambio("euro a dolar", 1.17);
 
+// Variables
+let pesos;
+let dolares;
+let euros;
+let inputPesos = document.getElementById("pesos");
+let inputDolares = document.getElementById("dolares"); 
+let inputEuros = document.getElementById("euros");
+
+// Array con los distintos tipos de cambio
+const divisas = [cambio1, cambio2, cambio3, cambio4];
+
 // Main del simulador
-opcion = prompt("Escriba la moneda que desea convertir: \n-Pesos argentinos.\n-Dólares.\n-Euros").toLowerCase();
-
-switch(opcion){
-    case "pesos":
-    case "pesos argentinos":
-        pesos = parseFloat(prompt("Ingrese cúantos pesos quiere convertir."));
-        document.getElementById("pesos").value = pesos.toFixed(2);
-        document.getElementById("dolares").value = convertirPesoDolar(pesos).toFixed(2);
-        document.getElementById("euros").value = convertirPesoEuro(pesos).toFixed(2);
-        break;
-
-    case "dólares":
-    case "dolares":
-    case "dólar":
-    case "dolar":
-        dolares = parseFloat(prompt("Ingrese cúantos dólares quiere convertir."));
-        document.getElementById("pesos").value = convertirDolarPeso(dolares).toFixed(2);
-        document.getElementById("dolares").value = dolares.toFixed(2);
-        document.getElementById("euros").value = convertirDolarEuro(dolares).toFixed(2);
-        break;
-    
-    case "euros":
-    case "euro":
-        euros = parseFloat(prompt("Ingrese cúantos euros quiere convertir."));
-        document.getElementById("pesos").value = convertirEuroPeso(euros).toFixed(2);
-        document.getElementById("dolares").value = convertirEuroDolar(euros).toFixed(2);
-        document.getElementById("euros").value = euros.toFixed(2);
-        break;
-    
-    default:
-        alert("Ingresaste una opción incorecta. Vuelve a intetarlo.");
-        break;
-}
-
 fecha.innerText = new Date().toLocaleDateString();
 
+inputPesos.addEventListener("click", mostrarPesosADivisas);
+function mostrarPesosADivisas(){
+    pesos = parseFloat(prompt("Ingrese cúantos pesos quiere convertir."));
+    document.getElementById("pesos").value = pesos.toFixed(2);
+    document.getElementById("dolares").value = convertirPesoDolar(pesos).toFixed(2);
+    document.getElementById("euros").value = convertirPesoEuro(pesos).toFixed(2);
+}
 
+inputDolares.addEventListener("click", mostrarDolaresADivisas);
+function mostrarDolaresADivisas (){
+    dolares = parseFloat(prompt("Ingrese cúantos dólares quiere convertir."));
+    document.getElementById("pesos").value = convertirDolarPeso(dolares).toFixed(2);
+    document.getElementById("dolares").value = dolares.toFixed(2);
+    document.getElementById("euros").value = convertirDolarEuro(dolares).toFixed(2);
+}
 
+inputEuros.addEventListener("click", mostrarEurosADivisas);
+function mostrarEurosADivisas (){
+    euros = parseFloat(prompt("Ingrese cúantos euros quiere convertir."));
+    document.getElementById("pesos").value = convertirEuroPeso(euros).toFixed(2);
+    document.getElementById("dolares").value = convertirEuroDolar(euros).toFixed(2);
+    document.getElementById("euros").value = euros.toFixed(2);
+}
